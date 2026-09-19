743650b53c5341c7ce95b5214ec621a362ddde51
AWulli <a.wulfert@protonmail.com>
2026-09-19 20:19:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication
-
-from classes.ui_backend import UiBackend
-
+from ui_backend import UiBackend
 if __name__ == "__main__":
     app = QApplication()
     ui = UiBackend()
